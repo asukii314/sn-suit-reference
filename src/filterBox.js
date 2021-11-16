ae0bf38b77af999a357af4495ba41c2d80fda392
Asukii <asukii314.twitch@gmail.com>
2021-11-15 22:48:57 -0500

    open filters by default

--- src/filterBox.js
+++ src/filterBox.js
@@ -5,7 +5,7 @@ export default class FilterBox extends Component {
     constructor(props){
         super(props);
         this.state = {
-            expanded: false
+            expanded: true
         };
     }
 
