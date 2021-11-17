b6b825b99a799061520b3161f44a1ac6f4e54400
Asukii <asukii314.twitch@gmail.com>
2021-11-17 01:02:10 -0500

    arrow key navigate through suits

--- src/suits.js
+++ src/suits.js
@@ -1,12 +1,25 @@
 import { Component } from 'react';
 import 'whatwg-fetch';
 import './suits.css';
+import ArrowKeysReact from 'arrow-keys-react';
 
 export default class SuitCards extends Component {
     constructor(props){
         super(props);
         this.state = {
         };
+        ArrowKeysReact.config({
+          left: () => this.nextSuit({forward: false}),
+          right: () => this.nextSuit({forward: true})
+        });
+    }
+
+    nextSuit = ({forward=true}={}) => {
+        if(!this.props.activeSuit) return;
+        const curSuitIdx = this.props.suits.map(suit => suit.name).indexOf(this.props.activeSuit.name);
+        const newSuitIdx = curSuitIdx + (forward ? 1 : -1);
+        if(newSuitIdx < 0 || newSuitIdx >= this.props.suits.length) return;
+        this.props.setActiveSuit(this.props.suits[newSuitIdx]);
     }
 
     onClick = (suit) => {
@@ -15,8 +28,12 @@ export default class SuitCards extends Component {
 
     renderSuitCard = (suit, idx) => {
         return (
-
-            <div className={`suit-card${suit === this.props.activeSuit ? ' active' : ''}`} key={idx} onClick={this.onClick.bind(this,suit)}>
+            <div
+                className={`suit-card${suit === this.props.activeSuit ? ' active' : ''}`}
+                key={idx}
+                onClick={this.onClick.bind(this,suit)}
+                {...ArrowKeysReact.events} tabIndex="1"
+            >
                 <div className='suit-icon-container'>
                     <img src={`rarity/${suit.rarity.toLowerCase()}.png`} className='suit-icon' alt='rarity' />
                     {suit.attribute && <img src={`attribute/${suit.attribute.toLowerCase()}.png`} className='suit-icon' alt='rarity' />}
