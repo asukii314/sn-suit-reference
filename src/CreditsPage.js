e9e536ef68b22e7e651430d9801c20edcaf01af0
Asukii <asukii314.twitch@gmail.com>
2022-01-22 23:42:45 -0500

    update credits

--- src/CreditsPage.js
+++ src/CreditsPage.js
@@ -15,21 +15,39 @@ export default function CreditsPage() {
 
         <b>SN Reference Images</b>
         <ul>
+            <li><a className='inline' href='https://nikki4.com.tw/newslist'>Official TW News</a></li>
+            <li>Official Twitter accounts for <a className='inline' href='https://twitter.com/ShiningNikki_JP'>Japan</a> and <a className='inline' href='https://twitter.com/ShiningNikki_SN'>Global</a></li>
+            <li>Official Facebook accounts for <a className='inline' href='https://www.facebook.com/ShiningNikkiTW/'>Taiwan</a> and <a className='inline' href='https://www.facebook.com/shiningnikki.vietnam'>Vietnam</a></li>
             <li><a className='inline' href='https://lensdump.com/rumi/albums'>Rumi's Lensdump</a> <i>(suit images)</i></li>
-            <li><a className='inline' href='https://lensdump.com/sakushi/albums/'>Saku's Lensdump</a> <i>(reflection art)</i></li>
+            <li><a className='inline' href='https://lensdump.com/sakushi/albums/'>Saku's Lensdump</a> and <a className='inline' href='https://lensdump.com/a/iKQN9'>CrystalRain's lensdump</a> <i>(full reflection art)</i></li>
             <li><a className='inline' href='https://lensdump.com/a/W7Tra/sub'>SND Lensdump</a> <i>(reflection icons)</i></li>
             <li><a className='inline' href='https://orangecatty-shiningnikki.tumblr.com/'>Orangecatty's Tumblr</a></li>
             <li><a className='inline' href='https://twitter.com/rei_sh3'>R E I - Twitter</a></li>
-            <li><a className='inline' href='https://nikki4.com.tw/newslist'>Official TW News</a></li>
-            <li>Official twitter accounts for <a className='inline' href='https://twitter.com/ShiningNikki_JP'>Japan</a> and <a className='inline' href='https://twitter.com/ShiningNikki_SN'>Global</a></li>
-            <li>Various posts on the official <a className='inline' href='https://discord.gg/shiningnikki'>SN Discord</a></li>
+            <li>Various posts on the official <a className='inline' href='https://discord.gg/shiningnikki'>Shining Nikki Discord</a> <i>(in #future-events)</i></li>
         </ul>
+
+
+        <b>Event Release Info</b>
+        <ul>
+            <li>Official Twitter accounts for <a className='inline' href='https://twitter.com/ShiningNikki_JP'>Japan</a> and <a className='inline' href='https://twitter.com/ShiningNikki_SN'>Global</a></li>
+            <li>Official Facebook accounts for <a className='inline' href='https://www.facebook.com/ShiningNikkiTW/'>Taiwan</a> and <a className='inline' href='https://www.facebook.com/shiningnikki.vietnam'>Vietnam</a></li>
+            <li>#newsflash channels in <a className='inline' href='https://discord.gg/yDJfk5q'>Shining Nikki Discord (SND)</a></li>
+            <li><a className='inline' href='https://orangecatty-shiningnikki.tumblr.com/'>Orangecatty's Tumblr</a></li>
+            <li><a className='inline' href='https://docs.google.com/spreadsheets/d/1OyUo4VS1FKWqOvAdD7c48EgtIOeo8o-c_yOAoUQPTyI/edit#gid=1851570774'>Shining Nikki TN Event Calendar</a> by Jeanne#2998</li>
+            <li><a className='inline' href='https://docs.google.com/spreadsheets/d/1kwS5WTBxfHhDCliR1BTdcTRtKF-dQVSncYcybvCpdB0/edit#gid=326770706'>Shining Nikki JP Calendar</a> by noodle oreo#1037</li>
+        </ul>
+
+
+        <b>Suit Metadata</b>
+        <ul>
+            <li><a className='inline' href='https://docs.google.com/spreadsheets/d/12bhpoIReHTVyQjNewiodA4rhL7zUSAhmq4i-5WV5BiU/edit#gid=0'>Suit Archive sheet</a> by luckynozomi#2333</li>
+            <li>Reflection skill data from Kami's <a className='inline' href='https://docs.google.com/spreadsheets/d/1Va9SCIrxni6TUEB1DFxwQK8Vj9mnsCxztsPA9OJja-g/'>Shadow Ranking Sheet</a></li>
+            <li>Metadata for global suits from the <a className='inline' href='https://shining-nikki.fandom.com/wiki/Shining_Nikki_Wiki'>Shining Nikki Wiki</a></li>
+        </ul>
+
 
         <b>Misc/Other</b>
         <ul>
-            <li>Reflection skill data from Kami's <a className='inline' href='https://docs.google.com/spreadsheets/d/1Va9SCIrxni6TUEB1DFxwQK8Vj9mnsCxztsPA9OJja-g/'>Shadow Ranking Sheet</a></li>
-            <li>Metadata for global suits from <a className='inline' href='https://shining-nikki.fandom.com/wiki/Shining_Nikki_Wiki'>the Shining Nikki Wiki</a></li>
-            <li>Help finding/organizing suit data from Demonic Dragon#3189</li>
             <li>Icons made by <a className='inline' href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" className='inline' title="Flaticon">www.flaticon.com</a></li>
             <li>And of course, <a className='inline' href="#/contact">me (asukii)</a> to pull it all together :)</li>
         </ul>
