584aeef15065d9bf186fc454bd6fbc4e4c03ea6c
Asukii <asukii314.twitch@gmail.com>
2022-03-20 17:35:10 -0400

    add nation to suit card + show icon info on hover

--- src/suits.js
+++ src/suits.js
@@ -34,37 +34,62 @@ export default class SuitCards extends Component {
             >
                 <div className='suit-icon-container'>
                     <div className='favourite-icon-container'>
-                        {!this.props.favouriteSuits?.includes(suit.id) && <img
-                            src='heart_black.png'
-                            className='action-bar-icon unfavourited suit-icon shadowed'
-                            alt='Favourite'
-                            title='Favourite'
-                            onClick={(e) => this.props.favourite(suit,e)}
-                        />}
-                        {this.props.favouriteSuits?.includes(suit.id) && <img
-                            src='heart_red.png'
-                            className='action-bar-icon favourited suit-icon shadowed'
-                            alt='Unfavourite'
-                            title='Unfavourite'
-                            onClick={(e) => this.props.unfavourite(suit,e)}
-                            />}
 
-                        {!this.props.ownedSuits?.includes(suit.id) &&
-                            <img src='tick-empty.png'
-                            className='action-bar-icon owned-icon suit-icon shadowed'
-                            alt='Mark as owned'
-                            title='Mark as owned'
-                            onClick={(e) => this.props.setOwned(suit,e)}
+                        {/* Favourite icon */}
+                        <div className='favourite-icon-with-label'>
+                            {!this.props.favouriteSuits?.includes(suit.id) && <img
+                                src='heart_black.png'
+                                className='action-bar-icon unfavourited suit-icon shadowed'
+                                alt='Favourite'
+                                title='Favourite'
+                                onClick={(e) => this.props.favourite(suit,e)}
                             />}
-                        {this.props.ownedSuits?.includes(suit.id) &&
-                            <img src='tick-filled.png'
-                            className='action-bar-icon owned-icon suit-icon shadowed green'
-                            alt='Unmark as owned'
-                            title='Unmark as owned'
-                            onClick={(e) => this.props.setNotOwned(suit,e)}
+                            {this.props.favouriteSuits?.includes(suit.id) && <img
+                                src='heart_red.png'
+                                className='action-bar-icon favourited suit-icon shadowed'
+                                alt='Unfavourite'
+                                title='Unfavourite'
+                                onClick={(e) => this.props.unfavourite(suit,e)}
+                                />}
+                            <div className='icon-label'>{this.props.favouriteSuits?.includes(suit.id) ? 'Saved to wishlist' : 'Not on wishlist'}</div>
+                        </div>
+
+                        {/* Owned icon */}
+                        <div className='favourite-icon-with-label'>
+                            {!this.props.ownedSuits?.includes(suit.id) &&
+                                <img src='tick-empty.png'
+                                className='action-bar-icon owned-icon suit-icon shadowed'
+                                alt='Mark as owned'
+                                title='Mark as owned'
+                                onClick={(e) => this.props.setOwned(suit,e)}
                             />}
-                        <img src={`rarity/${suit.rarity.toLowerCase()}.png`} className='suit-icon shadowed' alt='rarity' />
-                        {suit.attribute && <img src={`attribute/${suit.attribute.toLowerCase()}.png`} className='suit-icon' alt='rarity' />}
+                            {this.props.ownedSuits?.includes(suit.id) &&
+                                <img src='tick-filled.png'
+                                className='action-bar-icon owned-icon suit-icon shadowed green'
+                                alt='Unmark as owned'
+                                title='Unmark as owned'
+                                onClick={(e) => this.props.setNotOwned(suit,e)}
+                            />}
+                            <div className='icon-label'>{this.props.ownedSuits?.includes(suit.id) ? 'Owned' : 'Not owned'}</div>
+                        </div>
+
+                        {/* Rarity icon */}
+                        {suit.rarity && <div className='favourite-icon-with-label'>
+                            <img src={`rarity/${suit.rarity.toLowerCase()}.png`} className='suit-icon shadowed' alt='rarity' />
+                            <div className='icon-label'>{suit.rarity.toUpperCase()} rarity</div>
+                        </div>}
+
+                        {/* Attribute icon */}
+                        {suit.attribute && <div className='favourite-icon-with-label'>
+                            {suit.attribute && <img src={`attribute/${suit.attribute.toLowerCase()}.png`} className='suit-icon' alt='attribute' />}
+                            <div className='icon-label'>{suit.attribute} attribute</div>
+                        </div>}
+
+                        {/* Nation icon */}
+                        {suit.nation && <div className='favourite-icon-with-label'>
+                            {suit.nation && <img src={`nation/${suit.nation.toLowerCase()}.png`} className='suit-icon' alt='nation' />}
+                            <div className='icon-label'>{suit.nation} nation</div>
+                        </div>}
                     </div>
                 </div>
                 <img src={suit.images.promo} className='suit-img' alt={suit.name}/>
